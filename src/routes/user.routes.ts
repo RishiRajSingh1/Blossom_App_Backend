6ce4fae9a1cb4bd56ec9034a9aabd6b149092958
Rishi Raj Singh <78776240+RishiRajSingh1@users.noreply.github.com>
2023-10-27 03:13:24 +0530

    2nd Commit

--- src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { createUser, loginUser } from "../controllers/user.controller"
-
-const userRoutes = express.Router()
-
-userRoutes.route("/create").post(createUser)
-userRoutes.route("/login").post(loginUser)
-
-export default userRoutes
